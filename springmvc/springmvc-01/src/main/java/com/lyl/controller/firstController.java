3f8da74dd1306d02053062565b3a2cd2854954a7
lyl <13416088820@163.com>
2025-07-17 18:03:02 +0800

    7-17

---
+++ springmvc/springmvc-01/src/main/java/com/lyl/controller/firstController.java
@@ -0,0 +1,14 @@
+package com.lyl.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ *ClassName: firstController
+ *Package: com.lyl.controller
+ *Description:创建于 2025/7/17 17:27
+ *@Author lyl
+ *@Version 1.0
+ */
+@Controller
+public class firstController {
+}
