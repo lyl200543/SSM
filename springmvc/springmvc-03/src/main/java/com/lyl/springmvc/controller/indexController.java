39c999279f25bf309261e0670dad641696adce62
lyl <13416088820@163.com>
2025-07-19 18:33:26 +0800

    7-19

--- springmvc/springmvc-03/src/main/java/com/lyl/springmvc/controller/indexController.java
+++ springmvc/springmvc-03/src/main/java/com/lyl/springmvc/controller/indexController.java
@@ -1,7 +1,10 @@
 package com.lyl.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  *ClassName: indexController
@@ -12,8 +15,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class indexController {
-    @RequestMapping("/")
-    public String index(){
+    @RequestMapping ("/")
+    public String index () {
         return "index";
+    }
+
+    @RequestMapping (value = {"/test1" , "/test2"})
+    public String testValueArray () {
+        return "test";
+    }
+
+    //    @RequestMapping("/x?z")
+//    @RequestMapping("/x*z")
+    @RequestMapping ("/a/**")
+    public String testAntValue () {
+        return "ok";
+    }
+
+    @RequestMapping ("/login/{a}/{b}")
+    public String testRestfulURL (
+            @PathVariable ("a")
+            String username ,
+            @PathVariable ("b")
+            String password
+    ) {
+        System.out.println("用户名：" + username + ",密码：" + password);
+        return "ok";
+    }
+
+//    @RequestMapping (value = "/user/login", method = RequestMethod.POST)
+    @PostMapping("/user/login")
+    public String testMethod () {
+        return "ok";
     }
 }
